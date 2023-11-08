c7fa3311f09e4836f472ae40c1ff35916f01c240
agung saputra <agungsaputradifh@gmail.com>
2023-11-08 17:24:03 +0700

    add function

---
+++ 03-function/script.js
@@ -0,0 +1,12 @@
+function tambah(a,b){
+    return a+b;
+}
+
+const hasilTambah = tambah(4,5);
+console.log(hasilTambah);
+
+const kali = (a,b) => {
+    return a*b;
+}
+const hasilKali = kali(2,5);
+console.log(hasilKali);
