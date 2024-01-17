6d2766d43491f419e2223408b9930923f250bbe2
Agung Saputra <agungsaputradifh@gmail.com>
2024-01-17 17:11:43 +0700

    fix bug

--- 27-fetch-refactor/script.js
+++ 27-fetch-refactor/script.js
@@ -72,7 +72,7 @@ function showDetail(m) {
                     <div class="col-md">
                         <ul class="list-group">
                             <li class="list-group-item"><h4>${m.Title}</h4></li>
-                            <li class="list-group-item"><strong>Director : </strong>${m.Director}<li>
+                            <li class="list-group-item"><strong>Director : </strong>${m.Director}</li>
                             <li class="list-group-item"><strong>Actors : </strong>${m.Actors}</li>
                             <li class="list-group-item"><strong>Writer : </strong>${m.Writer}</li>
                             <li class="list-group-item"><strong>Plot :</strong><br>${m.Plot}</li>
